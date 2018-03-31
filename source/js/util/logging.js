a96f04061e5f4d54aa26d18872a31a7d97089748
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-31 20:48:29 +0300

    adds util for logging

---
+++ source/js/util/logging.js
@@ -0,0 +1,14 @@
+var logging = function (message, object) {
+
+  var pageLog = document.querySelector('.log');
+  var newLogItem = document.createElement('div');
+
+  if (object) {
+    newLogItem.innerHTML = message + ': ' + JSON.stringify(object);
+    console.log(message, object);
+  } else {
+    newLogItem.innerHTML = message;
+    console.log(message);
+  }
+  pageLog.appendChild(newLogItem);
+};
