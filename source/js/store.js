95eb3afa4b3eb9eb45be4067ffe7a7272a765657
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-01 16:20:54 +0300

    update store

--- source/js/store.js
+++ source/js/store.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var serverReport = document.querySelector( '.view-stub__label' );
+  var serverLog = document.querySelector( '.view-stub__label' );
 
   function createNewEvent( evt, elem, data ) {
     var newEvent = new CustomEvent( evt, {
@@ -10,41 +10,43 @@
     elem.dispatchEvent( newEvent );
   };
 
+  var handlerChange = {
+    'data-input': new Promise( function( resolve, reject ) {
+        logging('Store отправляет данные на сервер');
+        try {
+          var result = server.sendToServer(action.data);
+        } catch (err) {
+          reject( new Error( 'Ошибка при отправке данных серверу' ) );
+        };
+        resolve( result );
+      })
+      .then( function( result ) {
+        logging( 'Store запрашивает данные с сервера' );
+        if (result) {
+          return server.queryToServer( 'report' );
+        }
+      })
+      .then( function ( report ) {
+        logging( 'Store вызывает событие изменения' );
+        createNewEvent( 'change', serverLog, report );
+      })
+      .catch( function( err ) {
+        logging( err );
+      })
+  }
+
   window.store = {
     handlerAction: function( action ) {
       logging( 'Store получил Action', action );
-      switch( action.name ) {
-        case 'data-input':
-          if (action.data) {
-            new Promise( function( resolve, reject ) {
-              logging('Store отправляет данные на сервер');
-              try {
-                var result = server.sendToServer(action.data);
-              } catch (err) {
-                reject( new Error( 'Ошибка при получении данных сервером' ) );
-              };
-              resolve( result );
-            })
-            .then( function( result ) {
-              logging( 'Store запрашивает данные с сервера' );
-              if (result) {
-                return server.queryToServer( 'report' );
-              }
-            })
-            .then( function ( report ) {
-              logging( 'Store вызывает событие изменения' );
-              createNewEvent( 'change', serverReport, report );
-            })
-            .catch( function( err ) {
-              logging( err );
-            });
-
-          } else {
-            logging( 'Action, полученный Store, содержит некоректные данные' );
-          }
-          break;
-        default:
+      if ( action.data ) {
+        if ( handlerChange[ action.name ] ) {
+          logging( 'Store обрабатывает полученное событие' );
+          handlerChange[ action.name ]();
+        } else {
           logging( 'Store не содержит инструкций для этого события' );
+        }
+      } else {
+        logging( 'Action, полученный Store, содержит некоректные данные' );
       }
     }
   };
