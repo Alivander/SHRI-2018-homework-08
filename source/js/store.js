696001a0c2d0f135888204fb66ceae620d095d11
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-01 17:12:08 +0300

    corrects store

--- source/js/store.js
+++ source/js/store.js
@@ -12,34 +12,40 @@
     elem.dispatchEvent( newEvent );
   };
 
+  var handlerChange = {
+    'input': function( action ) {
+      new Promise( function( resolve, reject ) {
+        logging('Store отправляет данные на сервер', action.data );
+        try {
+          var result = server.sendToServer( action.data );
+        } catch ( err ) {
+          reject( new Error( 'Ошибка при отправке данных серверу' ) );
+        };
+        resolve( result );
+      })
+      .then( function( result ) {
+        logging( 'Store запрашивает данные с сервера' );
+        if (result) {
+          return server.queryToServer( 'report' );
+        }
+      })
+      .then( function ( report ) {
+        logging( 'Store вызывает событие изменения' );
+        createNewEvent( 'change', interfaceBlocks.serverLog, report );
+      })
+      .catch( function( err ) {
+        logging( err );
+      })
+    }
+  }
+
   window.store = {
     handlerAction: function( action ) {
       logging( 'Store получил Action', action );
       if ( action.data ) {
-        if ( 'input' ) {
+        if ( handlerChange[ action.name ] ) {
           logging( 'Store обрабатывает полученное событие' );
-          new Promise( function( resolve, reject ) {
-            logging('Store отправляет данные на сервер');
-            try {
-              var result = server.sendToServer( action.data );
-            } catch ( err ) {
-              reject( new Error( 'Ошибка при отправке данных серверу' ) );
-            };
-            resolve( result );
-          })
-          .then( function( result ) {
-            logging( 'Store запрашивает данные с сервера' );
-            if (result) {
-              return server.queryToServer( 'report' );
-            }
-          })
-          .then( function ( report ) {
-            logging( 'Store вызывает событие изменения' );
-            createNewEvent( 'change', interfaceBlocks.serverLog, report );
-          })
-          .catch( function( err ) {
-            logging( err );
-          })
+          handlerChange[ action.name ]( action );
         } else {
           logging( 'Store не содержит инструкций для этого события' );
         }
