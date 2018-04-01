9b410a13f62a350ca4d03fa9ea542d1b4f959f60
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-01 15:24:35 +0300

    adds view.js

---
+++ source/js/view.js
@@ -0,0 +1,63 @@
+(function() {
+
+  var viewStub = document.querySelector( '.view-stub' );
+  var input = viewStub.querySelector( '.view-stub__input' );
+  var button = viewStub.querySelector( '.view-stub__apply' );
+  var serverReport = viewStub.querySelector( '.view-stub__label' );
+
+  var interfaceBlocks = [ serverReport ];
+
+  button.addEventListener( 'click', function( evt ) {
+    evt.preventDefault();
+
+    var value = input.value;
+    input.value = '';
+    var newAction = new Action( 'data-input', value );
+    logging( 'Создание нового Action в View', newAction );
+
+    logging( 'Передача нового Action из View в Dispatcher' );
+    dispatcher.dispatch( newAction );
+  });
+
+  function Action( name, data ) {
+    this.name = name;
+    this.data = data;
+  };
+
+  var render = {
+    report: function ( reportList ) {
+      var reportBlock = document.createDocumentFragment();
+
+      reportList.forEach(function( item ) {
+        var reportItem = document.createElement( 'div' );
+
+        reportItem.textContent = item;
+        reportBlock.appendChild( reportItem );
+      });
+
+      serverReport.innerHTML = '';
+      serverReport.appendChild( reportBlock );
+    }
+  };
+
+  serverReport.addEventListener('change', function( evt ) {
+    evt.preventDefault();
+    logging('View реагирует на изменение');
+
+    logging('View перерисовывает блок с новыми данными');
+    render.report( evt.detail );
+  });
+
+
+
+
+
+// componentDidMount: function() {
+//     store.bind( 'change', this.listChanged );
+// },
+//
+// componentWillUnmount: function() {
+//     store.unbind( 'change', this.listChanged );
+// },
+
+})();
