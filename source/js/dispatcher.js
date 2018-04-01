328366e6867482fcb33672a80d16887be852874b
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-01 15:24:51 +0300

    adds dispatcher.js

---
+++ source/js/dispatcher.js
@@ -0,0 +1,8 @@
+var dispatcher = {
+  dispatch: function( action ) {
+    logging( 'Получение Dispatcher нового Action' );
+
+    logging( 'Вызов обработчика Actione из Store' );
+    store.handlerAction(action);
+  }
+};
