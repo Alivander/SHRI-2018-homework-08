eb68271d58e4f99d9fd7072788102483fd72cc7e
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-18 16:26:33 +0300

    corrects emmiter

--- source/js/util/emmiter.js
+++ source/js/util/emmiter.js
@@ -2,7 +2,6 @@ var eventEmitter = ( function() {
   var events = {};
 
   function on( eventName, handler ) {
-    console.info( 'eventEmitter регистрирует handler для события', eventName );
     if (!events[ eventName ]) {
       events[ eventName ] = [];
     }
@@ -11,7 +10,6 @@ var eventEmitter = ( function() {
   };
 
   function off( eventName, handler ) {
-    console.info( 'eventEmitter удаляет handler для события', eventName )
     events[ eventName ] = events[ eventName ].filter( function( value ) {
       return value !== handler;
     });
@@ -19,13 +17,12 @@ var eventEmitter = ( function() {
   };
 
   function emit( eventName ) {
-    console.info( 'eventEmitter генерирует событие', eventName )
     if ( events[ eventName ] ) {
       events[ eventName ].forEach( function( handler ) {
         handler();
       });
     } else {
-      console.log( 'eventEmitter не содержит обработчиков для этого события' );
+      console.info( 'eventEmitter не содержит обработчиков для этого события' );
     }
   };
 
